44536276924cd1ceae361dc032479064abd3c1f2
m-meno <mackenzie.meno@gmail.com>
2025-06-02 14:04:30 -0600

    Added request routes and controllers

--- controllers/requestControllers.mjs
+++ controllers/requestControllers.mjs
@@ -1,14 +1,30 @@
 import Request from "../models/requestSchema.mjs";
 
+let createRequest = async (req, res) => {
+    const newRequest = await Request.insertOne(req.body);
+
+    res.status(201).json(newRequest)
+};
+
 let readRequests = async (req, res) => {
     const allRequests = await Request.find({});
     res.json(allRequests)
-}
+};
+
+let updatedRequest = async (req, res) => {
+    let updatedRequest = await Request.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
+
+    res.json(updatedRequest);
+};
+
 
 let deleteRequest = async (req, res) => {
     let deleteRequest = Request.findByIdAndDelete(req.params.id);
     res.json(deleteRequest)
 }
 
-export default {readRequests, deleteRequest};
+export default { createRequest, readRequests, updatedRequest, deleteRequest };
 
--- routes/requestRoutes.mjs
+++ routes/requestRoutes.mjs
@@ -3,11 +3,22 @@ import requestControllers from "../controllers/requestControllers.mjs";
 
 const router = express.Router();
 
-// @route: /api/requests
+// @route: POST /api/requests
+// @desc: create request route
+// @access: Private
+router.post("/", auth, requestControllers.createRequest)
+
+// @route: GET /api/requests
 // @desc: Get all requests
 // @access: Public
 router.get("/", requestControllers.readRequests);
 
-// @route: 
-// @desc:
-// @access: 
+// @route: PUT /api/requests
+// @desc: Update one request
+// @access: Private
+router.put("/:id", auth, requestControllers.updatedRequest)
+
+// @route: DELETE /api/requests
+// @desc: Delete one request
+// @access: Private
+router.delete("/:id", auth, requestControllers.deleteRequest)
